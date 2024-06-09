e5d51b7032c375e66b369e2e20e80b1eab5596c1
SumitKumarGodwan <sumitgod510@gmail.com>
2024-06-09 13:09:19 +0530

    SG-1, added npm cache clean command

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,10 +4,7 @@
 curl -sL https://rpm.nodesource.com/setup_20.x | sudo -E bash -
 sudo yum install -y nodejs
 
+sudo npm cache clean --force
 # Clean old application files with elevated permissions
 sudo rm -rf /home/ec2-user
 
-# Fix permissions for npm cache if it exists
-if [ -d "/home/ec2-user/.npm" ]; then
-  sudo chown -R ec2-user:ec2-user /home/ec2-user/.npm
-fi
