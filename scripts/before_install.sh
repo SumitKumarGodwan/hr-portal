ee547daa372fd9c86266af02871dc0f4b9e3ca62
SumitKumarGodwan <sumitgod510@gmail.com>
2024-06-09 13:05:26 +0530

    SG-1, give permission when only .npm file exists

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# Install Node.js and npm
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+
+# Install the latest Node.js and npm
+curl -sL https://rpm.nodesource.com/setup_20.x | sudo -E bash -
 sudo yum install -y nodejs
 
-# Clean old application files
+# Clean old application files with elevated permissions
 sudo rm -rf /home/ec2-user
 
-# Fix permissions for npm cache
-sudo chown -R 1000:1000 "/home/ec2-user/.npm"
-# sudo chown -R ec2-user:ec2-user /home/ec2-user/.npm
+# Fix permissions for npm cache if it exists
+if [ -d "/home/ec2-user/.npm" ]; then
+  sudo chown -R ec2-user:ec2-user /home/ec2-user/.npm
+fi
