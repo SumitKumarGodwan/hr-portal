e75698870bcdb33c802570e66ecd251be465fb34
SumitKumarGodwan <sumitgod510@gmail.com>
2024-06-09 13:12:39 +0530

    SG-1, added chown command

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,3 +8,7 @@ sudo npm cache clean --force
 # Clean old application files with elevated permissions
 sudo rm -rf /home/ec2-user
 
+# Fix permissions for npm cache if it exists
+if [ -d "/home/ec2-user/.npm" ]; then
+  sudo chown -R 1000:1000 "/home/ec2-user/.npm"
+fi
