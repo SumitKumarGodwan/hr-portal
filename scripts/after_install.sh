010ca60dd8655b1db9ac01d19a81f6d15220c2a0
SumitKumarGodwan <sumitgod510@gmail.com>
2024-06-09 13:29:51 +0530

    SG-1, script chnage for before install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir /home/ec2-user
 cd /home/ec2-user
 
 # Install dependencies
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Install the latest Node.js and npm
-curl -sL https://rpm.nodesource.com/setup_20.x | sudo -E bash -
-sudo yum install -y nodejs
+# Install node.js
+sudo apt-get install python-software-properties -y
+sudo apt-add-repository ppa:chris-lea/node.js -y
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 sudo npm cache clean --force
+
 # Clean old application files with elevated permissions
-sudo rm -rf /home/ec2-user
+# sudo rm -rf /home/ec2-user
 
 # Fix permissions for npm cache if it exists
-if [ -d "/home/ec2-user/.npm" ]; then
-  sudo chown -R 1000:1000 "/home/ec2-user/.npm"
-fi
+# if [ -d "/home/ec2-user/.npm" ]; then
+#   sudo chown -R 1000:1000 "/home/ec2-user/.npm"
+# fi
