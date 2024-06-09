b5b290bfbff3bffc96e4ce6dace470b14bc9359a
SumitKumarGodwan <sumitgod510@gmail.com>
2024-06-09 13:01:30 +0530

    SG-1,Fix permissions for npm cache

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,4 +6,6 @@ sudo yum install -y nodejs
 # Clean old application files
 sudo rm -rf /home/ec2-user
 
+# Fix permissions for npm cache
+sudo chown -R 1000:1000 "/home/ec2-user/.npm"
 # sudo chown -R ec2-user:ec2-user /home/ec2-user/.npm
