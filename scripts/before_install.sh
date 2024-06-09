b68aa57342b971372daa3a5c6a93300d1c6d0e9a
SumitKumarGodwan <sumitgod510@gmail.com>
2024-06-09 12:57:02 +0530

    SG-1, remove permission access to npm

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,4 +6,4 @@ sudo yum install -y nodejs
 # Clean old application files
 sudo rm -rf /home/ec2-user
 
-sudo chown -R ec2-user:ec2-user /home/ec2-user/.npm
+# sudo chown -R ec2-user:ec2-user /home/ec2-user/.npm
